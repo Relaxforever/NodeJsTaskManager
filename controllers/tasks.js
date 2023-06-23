8b34c971ca7fbbe2c77d9b78bfa9b2ddfef52f2a
Fidel Caicedo <ffcaicedoc@gmail.com>
2023-06-22 19:34:22 -0500

    Added more Basic NodeJS Rest Api Skeleton

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,8 @@
 const getAllTasks = (req, res) => { res.send('all tasks from the file')};
 
-const createTask = (req, res) => { res.send('create task')};
+const createTask = (req, res) => { res.json(req.body)};
 
-const getTask = (req, res) => { res.send('get single task')}; 
+const getTask = (req, res) => { res.json({id: req.params.id})}; 
 
 const updateTask = (req, res) => { res.send('update task')};
 
